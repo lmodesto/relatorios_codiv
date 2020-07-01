b598d00b82bfb92fc3924da5378b720f7eec024f
levimodesto <levimodesto.ti@gmail.com>
2020-07-01 17:37:21 -0300

    acertano o docker-compose com o .env

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var logger = require('./config/winston')
 const port = 3000;
 
 app.listen(port, function(){
-  logger.info('Servidor rodando na porta '+port);
+   logger.info('Servidor rodando na porta '+port);
+   logger.info(process.env.USER)
 });
